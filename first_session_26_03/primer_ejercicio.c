070eacc8cf743e32371193095141da24e8fd3e14
Tomas Lugo <ge52bar@mytum.de>
2020-03-26 20:24:58 -0600

    -First exercise and .gitignore

---
+++ first_session_26_03/primer_ejercicio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Size of unsigned char %lu\n", 8* sizeof(unsigned char));
+    printf("Size of char %lu\n\n", 8* sizeof(char));
+
+    printf("Size of unsigned short %lu\n", 8* sizeof(unsigned short));
+    printf("Size of signed short %lu\n\n", 8* sizeof(short));
+
+    printf("Size of unsigned int %lu\n", 8* sizeof(unsigned int));
+    printf("Size of signed int %lu\n\n", 8* sizeof(int));
+
+    printf("Size of unsigned long %lu\n", 8* sizeof(unsigned long));
+    printf("Size of signed long %lu\n\n", 8* sizeof(long));
+
+    printf("Size of float %lu\n\n", 8* sizeof(float));
+
+    printf("Size of double %lu\n\n", 8* sizeof(double));
+}
